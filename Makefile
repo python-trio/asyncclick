1f90c43d43bed8284ce333288101eb722714cb1d
Matthias Urlichs <matthias@urlichs.de>
2021-05-20 10:09:12 +0200

    Use SCM version

---
+++ Makefile
@@ -0,0 +1,16 @@
+#!/usr/bin/make -f
+
+PACKAGE=asyncclick
+ifneq ($(wildcard /usr/share/sourcemgr/make/py),)
+include /usr/share/sourcemgr/make/py
+# availabe via http://github.com/smurfix/sourcemgr
+
+else
+%:
+	@echo "Please use 'python setup.py'."
+	@exit 1
+endif
+
+pytest:
+	sudo tests/test.sh
+
--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup
 
 setup(
     name="asyncclick",
+    use_scm_version={"version_scheme": "guess-next-dev", "local_scheme": "dirty-tag"},
+    setup_requires=["setuptools_scm"],
     install_requires=[
         "colorama; platform_system == 'Windows'",
         "importlib-metadata; python_version < '3.8'",
